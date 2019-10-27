dc0e8e67579a1e1ca92d2abfb38bf1b953cfa8d0
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2019-10-27 15:19:02 +0100

    js dom caching

---
+++ app.js
@@ -0,0 +1,8 @@
+// cache DOM
+const pwdDisplayEl = document.getElementById('pwd-display');
+const pwdLengthEl= document.getElementById('pwd-length');
+const pwdUpppercaseEl = document.getElementById('pwd-uppercase');
+const pwdLowercaseEl = document.getElementById('pwd-lowercase');
+const pwdNumbersEl = document.getElementById('pwd-numbers');
+const pwdSymbolsEl = document.getElementById('pwd-symbols');
+const pwdGenerateEl = document.getElementById('pwd-generate');
